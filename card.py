4865ee497eedd98b1be164a23b0104fb4d979467
czuluagaz <139249956+czuluagaz@users.noreply.github.com>
2023-09-19 21:24:33 +0200

    Update card.py

--- card.py
+++ card.py
@@ -1,7 +1,4 @@
-1.1 Symbol
-In card.py:
+1.2 Card
+In the same file, create a class Card that inherits from Symbol and add an attribute:
 
-Create a class called Symbol with two attributes:
-
-color which is a string.
-icon which is a single character out of this list [♥, ♦, ♣, ♠].
+value which is a string (one of ['A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K'])
