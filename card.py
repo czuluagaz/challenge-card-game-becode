30b6f0eb52f198cad2108597602658f8d394c033
czuluagaz <139249956+czuluagaz@users.noreply.github.com>
2023-09-19 21:25:37 +0200

    needed card.py

--- card.py
+++ card.py
@@ -1,3 +1,11 @@
+1. A simple card
+1.1 Symbol
+In card.py:
+
+Create a class called Symbol with two attributes:
+
+color which is a string.
+icon which is a single character out of this list [♥, ♦, ♣, ♠].
 1.2 Card
 In the same file, create a class Card that inherits from Symbol and add an attribute:
 
